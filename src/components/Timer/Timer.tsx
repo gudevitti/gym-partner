f7b7cdf7549977dbd6000a179447769ef44370d2
Angelo Vitti <angelo.vitti@ab-inbev.com>
2023-09-17 18:53:56 -0300

    Timer now allows negative numbers

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -3,19 +3,27 @@ interface TimerProps {
 }
 
 const Timer = ({ seconds }: TimerProps) => {
-  const formatedSeconds = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
+  const absSeconds = Math.abs(seconds);
 
-  const minutes = Math.floor(seconds / 60) % 60;
+  const formatedSeconds =
+    absSeconds % 60 < 10 ? `0${absSeconds % 60}` : absSeconds % 60;
+
+  const minutes = Math.floor(absSeconds / 60) % 60;
   const formatedMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-  const hours = Math.floor(seconds / 3600);
+  const hours = Math.floor(absSeconds / 3600);
   const formatedHours = hours < 10 ? `0${hours}` : hours;
 
   const formatedTime = `${
     hours ? formatedHours + ":" : ""
   }${formatedMinutes}:${formatedSeconds}`;
 
-  return <>{formatedTime}</>;
+  return (
+    <>
+      {seconds < 0 && "-"}
+      {formatedTime}
+    </>
+  );
 };
 
 export default Timer;
