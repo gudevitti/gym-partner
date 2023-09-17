d7323e403d912d3ffcbe3a390763b49c74127937
Angelo Vitti <angelo.vitti@ab-inbev.com>
2023-09-17 13:20:19 -0300

    Reestructuring routes and setting up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
 import "./App.css";
 import {
-  LoaderFunction,
+  Link,
+  Outlet,
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
-import Home from "./pages/Home/Home.page";
-import WorkoutsStart from "./pages/WorkoutsStart/WorkoutsStart.page";
-import WorkoutsEdit from "./pages/WorkoutsEdit/WorkoutsEdit.page";
-import WorkoutsDetails, {
-  loader as workoutLoader,
-} from "./pages/WorkoutsDetails/WorkoutsDetails.page";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/workouts/start",
-    element: <WorkoutsStart />,
-  },
-  {
-    path: "/workouts/edit",
-    element: <WorkoutsEdit />,
-  },
-  {
-    path: "/workouts/results",
-    element: <h1>Results</h1>,
-  },
-  {
-    path: "/workouts/details/:id",
-    element: <WorkoutsDetails />,
-    loader: workoutLoader as LoaderFunction,
-  },
-]);
 
 function App() {
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+    <>
+      <nav>
+        <Link to="/">Training</Link>
+        <Link to="/workouts">Workouts</Link>
+        <Link to="/statistics">Statistics</Link>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      /*
+       Training page:
+        - Main training dashboard
+          - Workout of the day
+        - Workout session tracker
+        - Workout journal
+        - Exercise log
+    */
+      { path: "/", element: <div>Training</div> },
+      /*
+      Workout and exercise editing page:
+        - Edit Workouts
+        - Customzie Exercises
+        - Create Training plan
+    */
+      { path: "/workouts", element: <div>Workouts</div> },
+      /*
+      Statistics page:
+        - Workout history
+        - Performance analytics
+        - Progress tracker
+        - Fitness stats
+    */
+      { path: "/statistics", element: <div>Statistics</div> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
